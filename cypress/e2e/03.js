95930657922dffdfb34c1b33d8b1326ab4b68d8b
Bruno Del Piero <bdelpiero93@gmail.com>
2022-04-08 15:22:22 -0300

    exercise 03

--- cypress/e2e/03.js
+++ cypress/e2e/03.js
@@ -1,13 +1,9 @@
-// 📜 In this one you'll be using the following new built-in commands:
-// - type: https://docs.cypress.io/api/commands/type.html
-// - url: https://docs.cypress.io/api/commands/url.html
-// - window: https://docs.cypress.io/api/commands/window.html
-// - its: https://docs.cypress.io/api/commands/its.html
-//
-// 📜 You'll also probably want to use the following new commands from Cypress Testing Library:
-// - getByText: https://testing-library.com/docs/dom-testing-library/api-queries#bytext
-// - getByLabelText: https://testing-library.com/docs/dom-testing-library/api-queries#bylabeltext
-// - getByTestId: https://testing-library.com/docs/dom-testing-library/api-queries#bytestid
+import {build, fake} from 'test-data-bot'
+
+const userBuilder = build('User').fields({
+  username: fake(f => f.internet.userName()),
+  password: fake(f => f.internet.password()),
+})
 
 describe('anonymous calculator', () => {
   it('has the right title', () => {
@@ -31,43 +27,25 @@ describe('anonymous calculator', () => {
   })
 })
 
-// 🐨 create another describe block here for "registration"
-// 🐨 create an it block called "should register a new user"
-// 🐨 create a user object with a username and password (make it up)
-// 💰 Try this: `const user = {username: 'chucknorris', password: 'ineednopassword'}`
+describe('registration', () => {
+  it('should register a new user', () => {
+    const user = userBuilder()
 
-// 🐨 Then visit the app and do the following actions
-// 1. Click the register link
-// 2. Get the username input by the label "Username" and type `user.username`
-// 3. Get the password input by the label "Password" and type `user.password`
-// 4. Get the submit button by it's text and click it.
-// 5. Get the url (💰 cy.url()) and assert that it should equal `${Cypress.config().baseUrl}/`
-// 6. Get the window (💰 cy.window()) and assert that its localStorage.token (💰 cy.its('localStorage.token')) is a string (💰 cy.should('be.a', 'string')) 
-// 7. Get the 'username-display' element by it's data-testid (💰 cy.getByTestId('username-display')) and verify that it should "have.text" user.username
-
-// 💯 In our simple example, registering a user with the same username every
-// time is fine because our backend is very basic and doesn't even validate
-// username uniqueness. And in a perfect world we would be re-initializing our
-// database between every test. However that's typically not very feasible so
-// having a hard-coded user here could cause issues in a more real-world
-// scenario. So instead of hard-coding the user here, try to generate the user's
-// information. You could use `Math.random()` yourself, or you could use the
-// `test-data-bot` package which we have installed already.
-// 📜 https://github.com/jackfranklin/test-data-bot
-
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=cypress%20testing&e=03&em=
-*/
-describe('elaboration and feedback', () => {
-  it.skip('was submitted', () => {
-    const submitted = false // change this when you've submitted!
-    expect(submitted).to.be(true)
+    cy.visit('/')
+      .getByText(/register/i)
+      .click()
+      .getByLabelText('Username', {force: true})
+      .type(user.username)
+      .getByLabelText('Password')
+      .type(user.password)
+      .getByText(/submit/i)
+      .click()
+      .url()
+      .should('equal', `${Cypress.config().baseUrl}/`)
+      .window()
+      .its('localStorage.token')
+      .should('be.a', 'string')
+      .getByTestId('username-display')
+      .should('have.text', user.username)
   })
 })
-////////////////////////////////
